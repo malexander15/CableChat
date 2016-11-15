fe1fc11da6e84c876e5c6e93769fa321ef15d499
malexander15 <creepinjezus15>
2016-11-14 23:06:12 -0500

    Made MVC for cable action chat app

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -17,6 +17,7 @@ class ChatRoomsController < ApplicationController
     end
   def show
   @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+  @message = Message.new
   end
 
   private
